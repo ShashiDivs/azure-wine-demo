a074aeabc3f60a440f1f892c0d86eb4ec8b28d9a
“ShashiDivs” <shashipolity@gmail.com>
2026-03-02 22:40:56 +0530

    startup file updated

--- startup.sh
+++ startup.sh
@@ -1,13 +1,13 @@
+#!/bin/bash
+echo "Starting Wine Quality Classifier..."
 
-echo "Starting Wine Quality Classifier...."
-
-PORT = "${PORT:8000}"
-echo "Using PORT=${PORT}
+PORT="${PORT:-8000}"
+echo "Using PORT=${PORT}"
 
 exec python -m streamlit run app.py \
-    --server.port="${PORT}"
-    --server.address=0.0.0.0 \
-    --server.headless=true \
-    --server.enableCORS=false \
-    --server.enableXsrfProtection=false \
-    --server.gatherUsageStats=false
+  --server.port="${PORT}" \
+  --server.address=0.0.0.0 \
+  --server.headless=true \
+  --server.enableCORS=false \
+  --server.enableXsrfProtection=false \
+  --browser.gatherUsageStats=false
